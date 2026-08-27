0dea6cadef4effa609fb2254c70cf3d710e32e4c
Sunlake <register@milkcat.org>
2026-08-27 08:37:03 +0800

    feat(agentos): add capability adapter namespace

---
+++ capabilities/__init__.py
@@ -0,0 +1,5 @@
+"""AgentOS domain capability adapters.
+
+External libraries remain independent; modules here translate their observable
+behavior into AgentOS capability experience and state contracts.
+"""
